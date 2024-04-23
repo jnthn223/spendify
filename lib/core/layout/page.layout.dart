2f0cb2c9e42c268112ce5fb5727b30dfe76f5bb8
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2024-04-23 11:31:25 +0800

    update app bar and navigator style

--- lib/core/layout/page.layout.dart
+++ lib/core/layout/page.layout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:spendify/core/theme/app_pallete.dart';
+import 'package:spendify/core/theme/theme.dart';
 
 abstract class AppPage {
   String get pageTitle;
@@ -21,22 +23,46 @@ class PageLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(activePage.pageTitle),
-      ),
-      body: activePage.build(context),
-      bottomNavigationBar: BottomNavigationBar(
-        items: pages
-            .map((page) => BottomNavigationBarItem(
-                  icon: Icon(page.pageIcon),
-                  label: page.pageTitle,
-                ))
-            .toList(),
-        currentIndex: pages.indexOf(activePage),
-        onTap: (index) {
-          setActivePage(pages[index]);
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text(activePage.pageTitle), // Use the state variable for title
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Icon(Icons.person),
+            )
+          ],
+
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(2.0), // Adjust height as needed
+            child: Divider(
+              thickness: 2.0, // Adjust thickness as needed
+              color: Colors.black, // Customize border color
+            ),
+          ),
+        ),
+        body: activePage.build(context),
+        bottomNavigationBar: Container(
+          decoration: const BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.black, width: 2.0)),
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            enableFeedback: false,
+            selectedItemColor: AppPallete.accentColor1,
+            unselectedItemColor: AppTheme.getPrimaryTextColor(context),
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            items: pages
+                .map((page) => BottomNavigationBarItem(
+                      icon: Icon(page.pageIcon),
+                      label: page.pageTitle,
+                    ))
+                .toList(),
+            currentIndex: pages.indexOf(activePage),
+            onTap: (index) {
+              setActivePage(pages[index]);
+            },
+          ),
+        ));
   }
 }
